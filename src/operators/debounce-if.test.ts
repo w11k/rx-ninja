9d5b860287a3f3fa610fa7db0643932d101879e8
Sascha Engmann <engmann@w11k.de>
2020-05-10 10:22:18 +0200

    add formatter flag to Project.xml to prevent reformat of marble strings in test, add map-to-value-and-changed-properties documaentation and tests

--- src/operators/debounce-if.test.ts
+++ src/operators/debounce-if.test.ts
@@ -4,25 +4,28 @@ import { marbles } from "rxjs-marbles";
 describe("debounceIf", () => {
 
   it("should not debounce and emit values, when predicate evaluates to false", marbles(m => {
+    // @formatter:off
     const source =         m.cold("-1--2--3|");
     const expectedOutput = m.cold("-1--2--3|");
-
+    // @formatter:on
     const destination = source.pipe(debounceIf(m.time("--|"), () => false, m.scheduler));
     m.expect(destination).toBeObservable(expectedOutput);
   }));
 
   it("should debounce every value by one frame, when predicate evaluates to true", marbles(m => {
+    // @formatter:off
     const source =         m.cold("1-2-(3|)");
     const expectedOutput = m.cold("-1-2-(3|)");
-
+    // @formatter:on
     const destination = source.pipe(debounceIf(m.time("-|"), () => true, m.scheduler));
     m.expect(destination).toBeObservable(expectedOutput);
   }));
 
   it("should debounce only even values", marbles(m => {
+    // @formatter:off
     const source =         m.cold("1-2-(3|)");
     const expectedOutput = m.cold("1--2(3|)");
-
+    // @formatter:on
     const destination = source.pipe(debounceIf(m.time("-|"), isEven, m.scheduler));
     m.expect(destination).toBeObservable(expectedOutput);
   }));
