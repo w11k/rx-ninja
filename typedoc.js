019626dabc8dcea8443fd1a85fdf021cd64f8a16
Philipp Burgmer <burgmer@w11k.de>
2020-12-29 16:19:39 +0100

    Revert "update typescript and typedoc"
    This reverts commit 695bddeb

--- typedoc.js
+++ typedoc.js
@@ -1,7 +1,7 @@
 module.exports = {
+    excludeNotExported: true,
     excludePrivate: true,
     media: "./tmp/swirly/svgs",
-    entryPoints: ["src/index.ts"],
     out: "doc",
     exclude: ["**/*+(.test).ts", "src/tslint_rules/examples/**/*.ts", "**/*+(.test.diagram).ts"],
 };
